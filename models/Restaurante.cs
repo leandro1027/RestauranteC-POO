cad0a7e7224ffd03db4cd2fe5eb90b37b807b244
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-08-13 14:36:11 -0300

    .

--- models/Restaurante.cs
+++ models/Restaurante.cs
@@ -1,5 +1,13 @@
 namespace RestauranteC_POO;
 
+<<<<<<< Updated upstream
 public class Restaurante{
     
+=======
+
+public class Restaurante : Estabelecimento
+{
+   
+  
+>>>>>>> Stashed changes
 }
