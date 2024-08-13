f9a1a3a53fb3a36788799187c4ec3d8493c243b4
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-08-13 14:40:33 -0300

    concertei problemas de commits

--- models/Prato.cs
+++ models/Prato.cs
@@ -1,18 +1,5 @@
 namespace RestauranteC_POO;
 
-<<<<<<< Updated upstream
 public class Prato{
     
 }
-=======
-namespace RestauranteC_POO
-{
-    public class Prato
-    {
-        public string Nome { get; set; }
-        private decimal preco;
-        public bool Vegetariano { get; set; }
-
-      
-}
->>>>>>> Stashed changes
--- models/Restaurante.cs
+++ models/Restaurante.cs
@@ -1,13 +1,5 @@
 namespace RestauranteC_POO;
 
-<<<<<<< Updated upstream
 public class Restaurante{
     
-=======
-
-public class Restaurante : Estabelecimento
-{
-   
-  
->>>>>>> Stashed changes
 }
