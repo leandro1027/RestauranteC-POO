31c6d7d696432428f2b631f7c9e0c12b421ce3a0
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-08-14 11:26:38 -0300

    .

--- models/PedidoDelivery.cs
+++ models/PedidoDelivery.cs
@@ -2,9 +2,9 @@ namespace RestauranteC_POO;
 
 public class PedidoDelivery : Pedido
 {
-       private const decimal taxaEntrega = 10.00m;
+       private const decimal taxaEntrega = 10.00m; // taxa de entrega fixa
 
-        public override decimal CalcularTotal()
+        public override decimal CalcularTotal() //implementação do método abstrato herdado de Pedido
         {
             return Pratos.Sum(p => p.ObterPreco()) + taxaEntrega;
         }
--- models/PedidoPresencial.cs
+++ models/PedidoPresencial.cs
@@ -2,7 +2,7 @@ namespace RestauranteC_POO;
 
 public class PedidoPresencial : Pedido
 {
-      public override decimal CalcularTotal()
+      public override decimal CalcularTotal() //implementação do método abstrato herdado de Pedido
         {
             return Pratos.Sum(p => p.ObterPreco());
         }
